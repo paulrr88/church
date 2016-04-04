b612ff649dd241962f945b9f07fd000d82c42403
paulrr88 <Paul@Pauls-iMac.home>
2016-04-04 10:36:39 +0100

    Diary update - addition of Discipleship Explored

--- diary.php
+++ diary.php
@@ -6,7 +6,7 @@
 
 				<p class="centre">(For a long-term look at our church, see the <a href="upcoming.php">Coming Weeks page</a>)</p>
 
-				<p class="centre"><strong>Schedule for the week 27th March to 3rd April</strong></p>
+				<p class="centre"><strong>Schedule for the week 3rd to 10th April</strong></p>
 
 				<table class="styled" summary="Schedule for events in the coming week(s)">
 					<thead>
@@ -18,14 +18,39 @@
 					</thead>
 					<tbody>
 						<tr class="odd">
-							<th scope="row">Wednesday<br />3:00-4:00 P.M.</th>
-							<td>Prayer time</td>
+							<th scope="row">Tuesday<br /> 10:30 A.M.</th>
+							<td>Home Group at<br />Mike &amp; Pat's</td>
+							<td>Acts 17:1-15, 1 Thessalonians 3:1-5 - <br />"A New Church"</td>
+						</tr>
+            <tr>
+							<th scope="row">Tuesday<br />2:30 P.M.</th>
+							<td>Women's Fellowship:<br />Mr. David Napper</td>
 							<td></td>
 						</tr>
+						<!--temp--><tr class="odd">
+							<th scope="row">Tuesday<br />7:30 P.M.</th>
+							<td><strong>"Discipleship Explored"</strong></td>
+							<td>In the church</td>
+						</tr>
 						<tr>
+							<th scope="row">Wednesday<br />7:00 P.M.</th>
+							<td>Home Group at<br />David &amp; Audrey's</td>
+							<td></td>
+						</tr>
+						<tr class="odd">
+							<th scope="row">Thursday<br />2:00-4:00 P.M.</th>
+							<td>"Dabblers" Hobby Group</td>
+							<td>All welcome!</td>
+						</tr>
+						<tr>
+							<th scope="row">Friday<br />10:30 A.M.</th>
+							<td>Home Group at<br />Bryan &amp; Audrey's</td>
+							<td></td>
+						</tr>
+						<tr class="odd">
 							<th scope="row">Sunday<br />10:30 A.M.<br />6:30 P.M.</th>
 							<td>Sunday Services</td>
-							<td>Evening worship includes communion</td>
+							<td></td>
 						</tr>
 					</tbody>
 				</table>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 
 				<!--<h1 class="hcline"><a href="holidayclub.php">Holiday Club 2015 details and form now available!</a></h1>-->
 
-				<div class="nbox_top"><h1>Diary update - 28/03/16</h1></div>
+				<div class="nbox_top"><h1>Diary update - 04/04/16</h1></div>
 
 				<div class="nbox_content">
 					<p>I've <a href="diary.php">updated the diary</a> for the week.</p>
