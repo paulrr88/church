bf08ca8f0583f310124fe5ebff793f8bd3a19266
paulrr88 <Paul@Pauls-iMac.home>
2016-05-30 10:38:25 +0100

    Diary update

--- diary.php
+++ diary.php
@@ -6,7 +6,7 @@
 
 				<p class="centre">(For a long-term look at our church, see the <a href="upcoming.php">Coming Weeks page</a>)</p>
 
-				<p class="centre"><strong>Schedule for the week 22nd to 29th May</strong></p>
+				<p class="centre"><strong>Schedule for the week 29th May to 5th June</strong></p>
 
 				<table class="styled" summary="Schedule for events in the coming week(s)">
 					<thead>
@@ -18,24 +18,29 @@
 					</thead>
 					<tbody>
 						<tr class="odd">
-							<th scope="row">Tuesday<br />2:30 P.M.</th>
-							<td>Women's Fellowship:<br />Mrs. Gwyneth Klemenz</td>
-							<td></td>
-						</tr>
-						<!--temp--><tr>
-							<th scope="row">Tuesday<br />7:30 P.M.</th>
-							<td><strong>"Discipleship Explored"</strong></td>
-							<td>In the church</td>
-						</tr>
-						<tr class="odd">
-							<th scope="row">Wednesday<br />3:00-4:00 P.M.</th>
-							<td>Prayer time</td>
-							<td></td>
+							<th scope="row">Tuesday<br /> 10:30 A.M.</th>
+							<td>Home Group at<br />Mike &amp; Pat's</td>
+							<td>1 Thessalonians 4:1-5:11 - <br />"Living and Waiting for the Lord"</td>
 						</tr>
 						<tr>
+							<th scope="row">Wednesday<br />7:00 P.M.</th>
+							<td>Home Group at<br />David &amp; Audrey's</td>
+							<td></td>
+						</tr>
+						<tr class="odd">
+							<th scope="row">Thursday<br />2:00-4:00 P.M.</th>
+							<td>"Dabblers" Hobby Group</td>
+							<td>All welcome!</td>
+						</tr>
+						<tr>
+							<th scope="row">Friday<br />10:30 A.M.</th>
+							<td>Home Group at<br />Bryan &amp; Audrey's</td>
+							<td></td>
+						</tr>
+						<tr class="odd">
 							<th scope="row">Sunday<br />10:30 A.M.<br />6:30 P.M.</th>
 							<td>Sunday Services</td>
-							<td></td>
+							<td>Both services will be led by Rev. Paul Carter<br />Evening worship includes communion</td>
 						</tr>
 					</tbody>
 				</table>
