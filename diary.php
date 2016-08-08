eb74841140e696f489cd01ea9dd49f1f215bf0a6
paulrr88 <paul@Pauls-iMac.home>
2016-08-08 11:12:02 +0100

    Weekly update (two week diary)

--- diary.php
+++ diary.php
@@ -2,11 +2,11 @@
 
 <?php include("header.php"); ?>
 			<div id="content">
-				<h1>This Week's Diary</h1>
+				<h1>This Fortnight's Diary</h1>
 
 				<p class="centre">(For a long-term look at our church, see the <a href="upcoming.php">Coming Weeks page</a>)</p>
 
-				<p class="centre"><strong>Schedule for the week 31st July to 7th August</strong></p>
+				<p class="centre"><strong>Schedule for the weeks 7th to 21st August</strong></p>
 
 				<table class="styled" summary="Schedule for events in the coming week(s)">
 					<thead>
@@ -18,18 +18,39 @@
 					</thead>
 					<tbody>
 						<tr class="odd">
-							<th scope="row">Wednesday<br />3:00-4:00 P.M.</th>
+							<th scope="row">Tuesday 9th to Friday 12th<br />10:00 A.M. to<br />12:00 noon</th>
+							<td><strong>"Rocky's Plaice" Holiday Club</strong></td>
+							<td>See our <a href="holidayclub.php">Holiday Club page</a><br />for details!</td>
+						</tr>
+						<tr>
+							<th scope="row">Friday 12th<br />6:00 P.M.</th>
+							<td><strong>Finale and Buffet</strong></td>
+							<td>All welcome! Tickets £3 for adults,<br />£1.50 for children,<br />please purchase during the week</td>
+						</tr>
+						<tr class="odd">
+							<th scope="row">Sunday 14th<br />10:30 A.M.<br />6:30 P.M.</th>
+							<td>Sunday Services</td>
+							<td></td>
+						</tr>
+						<tr>
+							<th scope="row">Tuesday 16th<br />2:30 P.M.</th>
+							<td>Women's Fellowship:<br />Tea &amp; Chat/Bring &amp; Buy Sale</td>
+							<td></td>
+						</tr>
+						<tr class="odd">
+							<th scope="row">Wednesday 17th<br />3:00-4:00 P.M.</th>
 							<td>Prayer time</td>
 							<td></td>
 						</tr>
 						<tr>
-							<th scope="row">Sunday<br />10:30 A.M.<br />6:30 P.M.</th>
+							<th scope="row">Saturday 20th<br /> 9:00 A.M.</th>
+							<td><strong>Men's Breakfast</strong><br />at the Henty Arms</td>
+							<td>(Please sign up on list in foyer)</td>
+						</tr>
+						<tr class="odd">
+							<th scope="row">Sunday 21st<br />10:30 A.M.<br />6:30 P.M.</th>
 							<td>Sunday Services</td>
-							<td>Morning service will be led by<br />
-							Rev. Trevor Smetham<br />
-							Evening service will be led by <br />
-							Mr. Leslie Voke<br />
-							Evening worship includes communion</td>
+							<td>Morning worship includes communion</td>
 						</tr>
 					</tbody>
 				</table>
