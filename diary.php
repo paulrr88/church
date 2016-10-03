9dc017f28f512e2e09acaa15cfc8bb6552ee69aa
paulrr88 <paul@Pauls-iMac.home>
2016-10-03 10:48:29 +0100

    Diary and news sheet

--- diary.php
+++ diary.php
@@ -2,11 +2,11 @@
 
 <?php include("header.php"); ?>
 			<div id="content">
-				<h1>This Fortnight's Diary</h1>
+				<h1>This Week's Diary</h1>
 
 				<p class="centre">(For a long-term look at our church, see the <a href="upcoming.php">Coming Weeks page</a>)</p>
 
-				<p class="centre"><strong>Schedule for the weeks 18th September to 2nd October</strong></p>
+				<p class="centre"><strong>Schedule for the week 2nd to 9th October</strong></p>
 
 				<table class="styled" summary="Schedule for events in the coming week(s)">
 					<thead>
@@ -18,69 +18,45 @@
 					</thead>
 					<tbody>
 						<tr class="odd">
-							<th scope="row">Tuesday 20th<br />2:30 P.M.</th>
-							<td>Women's Fellowship:<br />London City Mission: Open Afternoon</td>
-							<td>All welcome, men included!</td>
+							<th scope="row">Tuesday<br />2:30 P.M.</th>
+							<td>Women's Fellowship:<br />Mrs. Pam Weaver</td>
+							<td></td>
 						</tr>
 						<tr>
-							<th scope="row">Wednesday 21st<br />7:00 P.M.</th>
+							<th scope="row">Wednesday<br />7:00 P.M.</th>
 							<td>Home Group at<br />David &amp; Audrey's</td>
 							<td></td>
 						</tr>
 						<tr class="odd">
-							<th scope="row">Thursday 22nd<br />2:00-4:00 P.M.</th>
+							<th scope="row">Thursday<br />2:00-4:00 P.M.</th>
 							<td>"Dabblers" Hobby Group</td>
 							<td>All welcome!</td>
 						</tr>
 						<tr>
-							<th scope="row">Friday 23rd<br />10:30 A.M.</th>
+							<th scope="row">Friday<br />10:30 A.M.</th>
 							<td>Home Group at<br />Bryan &amp; Audrey's</td>
 							<td></td>
 						</tr>
 						<tr class="odd">
-							<th scope="row">Friday 23rd<br />4:45-5:45 P.M.</th>
+							<th scope="row">Friday<br />4:45-5:45 P.M.</th>
 							<td>Friday Club<br />(Ages 4-7)</td>
 							<td></td>
 						</tr>
 						<tr>
-							<th scope="row">Friday 23rd<br />6:00-7:00 P.M.</th>
+							<th scope="row">Friday<br />6:00-7:00 P.M.</th>
 							<td>Friday Club<br />(Ages 8-11)</td>
 							<td></td>
 						</tr>
 						<tr class="odd">
-							<th scope="row">Sunday 25th<br />10:30 A.M.<br />6:30 P.M.</th>
+							<th scope="row">Saturday<br /> 8:45 A.M.</th>
+							<td><strong>Women's Breakfast</strong>:<br />Lesley Shayler</td>
+							<td>(Ticket-holders only)</td>
+						</tr>
+						<tr>
+							<th scope="row">Sunday<br />10:30 A.M.<br />6:30 P.M.</th>
 							<td>Sunday Services</td>
-							<td></td>
-						</tr>
-						<tr>
-							<th scope="row">Tuesday 27th<br />2:30 P.M.</th>
-							<td>Women's Fellowship:<br />Miss Pam Evans</td>
-							<td></td>
-						</tr>
-						<tr class="odd">
-							<th scope="row">Wednesday 28th<br />3:00-4:00 P.M.</th>
-							<td>Prayer time</td>
-							<td></td>
-						</tr>
-						<tr>
-							<th scope="row">Thursday 29th<br /> 10:30 A.M.</th>
-							<td>Home Group at<br />Mike &amp; Pat's</td>
-							<td>Matthew 9:35-38; John 4:7-15 - <br />"Sheep without a Shepherd"</td>
-						</tr>
-						<tr class="odd">
-							<th scope="row">Friday 30th<br />4:45-5:45 P.M.</th>
-							<td>Friday Club<br />(Ages 4-7)</td>
-							<td></td>
-						</tr>
-						<tr>
-							<th scope="row">Friday 30th<br />6:00-7:00 P.M.</th>
-							<td>Friday Club<br />(Ages 8-11)</td>
-							<td></td>
-						</tr>
-						<tr class="odd">
-							<th scope="row">Sunday 2nd<br />10:30 A.M.<br />6:30 P.M.</th>
-							<td>Sunday Services</td>
-							<td></td>
+							<td>N.B. evening is a fellowship evening<br />
+								(pastor at SEBA celebration)</td>
 						</tr>
 					</tbody>
 				</table>
