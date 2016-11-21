d2bbf0faa5b5899163656f85fda3787977c1e19b
paulrr88 <paul@Pauls-iMac.home>
2016-11-21 10:13:25 +0000

    Weekly update

--- diary.php
+++ diary.php
@@ -6,7 +6,7 @@
 
 				<p class="centre">(For a long-term look at our church, see the <a href="upcoming.php">Coming Weeks page</a>)</p>
 
-				<p class="centre"><strong>Schedule for the week 13th to 20th November</strong></p>
+				<p class="centre"><strong>Schedule for the week 20th to 27th November</strong></p>
 
 				<table class="styled" summary="Schedule for events in the coming week(s)">
 					<thead>
@@ -18,45 +18,34 @@
 					</thead>
 					<tbody>
 						<tr class="odd">
-							<th scope="row">Tuesday<br />2:30 P.M.</th>
-							<td>Women's Fellowship:<br />Miss Olive McCarten</td>
-							<td></td>
+							<th scope="row">Tuesday<br />2:15 P.M.</th>
+							<td><strong>Women's Fellowship:<br />Mr. &amp; Mrs. Lambert (China)</strong></td>
+							<td>Open meeting, all welcome, men included!</td>
 						</tr>
 						<tr>
-							<th scope="row">Wednesday<br />7:00 P.M.</th>
-							<td>Home Group at<br />David &amp; Audrey's</td>
+							<th scope="row">Wednesday<br />3:00-4:00 P.M.</th>
+							<td>Prayer time</td>
 							<td></td>
 						</tr>
 						<tr class="odd">
-							<th scope="row">Thursday<br />10:00 A.M. -<br />
-								12:00 noon</th>
-							<td>"Dabblers" Open Morning</td>
-							<td>All welcome!</td>
+							<th scope="row">Thursday<br /> 10:30 A.M.</th>
+							<td>Home Group at<br />Mike &amp; Pat's</td>
+							<td>Luke 13:31-35 &amp; 19:32-44 - <br />"Sorrow for Jerusalem"</td>
 						</tr>
 						<tr>
-							<th scope="row">Friday<br />10:30 A.M.</th>
-							<td>Home Group at<br />Bryan &amp; Audrey's</td>
-							<td></td>
-						</tr>
-						<tr class="odd">
 							<th scope="row">Friday<br />4:45-5:45 P.M.</th>
 							<td>Friday Club<br />(Ages 4-7)</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr class="odd">
 							<th scope="row">Friday<br />6:00-7:00 P.M.</th>
 							<td>Friday Club<br />(Ages 8-11)</td>
 							<td></td>
 						</tr>
-						<tr class="odd">
-							<th scope="row">Saturday<br /> 9:00 A.M.</th>
-							<td>Men's Breakfast<br />at the Henty Arms</td>
-							<td>(Please sign up on list in foyer)</td>
-						</tr>
 						<tr>
-							<th scope="row">Sunday<br />10:30 A.M.<br />6:30 P.M.</th>
+							<th scope="row">Advent Sunday<br />10:30 A.M.<br /><em>4:00 P.M.</em></th>
 							<td>Sunday Services</td>
-							<td>Morning worship includes communion</td>
+							<td><strong>Joint service at St. Andrew's<br />in evening, no service here</strong></td>
 						</tr>
 					</tbody>
 				</table>
