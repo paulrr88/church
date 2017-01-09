2213de63561ce642777aa391bb2ba2fe938bbabb
paulrr88 <paul@Pauls-iMac.home>
2017-01-09 09:54:17 +0000

    Weekly update sans news sheet!

--- diary.php
+++ diary.php
@@ -6,7 +6,7 @@
 
 				<p class="centre">(For a long-term look at our church, see the <a href="upcoming.php">Coming Weeks page</a>)</p>
 
-				<p class="centre"><strong>Schedule for the week 1st to 8th January</strong></p>
+				<p class="centre"><strong>Schedule for the week 8th to 15th January</strong></p>
 
 				<table class="styled" summary="Schedule for events in the coming week(s)">
 					<thead>
@@ -17,20 +17,35 @@
 						</tr>
 					</thead>
 					<tbody>
-						<tr>
-							<th scope="row">Wednesday<br />3:00-4:00 P.M.</th>
-							<td>Prayer time</td>
+						<tr class="odd">
+							<th scope="row">Wednesday<br />7:00 P.M.</th>
+							<td>Home Group at<br /><em>Edna's</em></td>
 							<td></td>
 						</tr>
+						<tr>
+							<th scope="row">Thursday<br />2:00-4:00 P.M.</th>
+							<td>"Dabblers" Hobby Group</td>
+							<td>All welcome!</td>
+						</tr>
 						<tr class="odd">
+							<th scope="row">Friday<br />10:30 A.M.</th>
+							<td>Home Group at<br />Bryan &amp; Audrey's</td>
+							<td></td>
+						</tr>
+						<tr>
 							<th scope="row">Friday<br />4:45-5:45 P.M.</th>
 							<td>Friday Club<br />(Ages 4-7)</td>
 							<td></td>
 						</tr>
-						<tr>
+						<tr class="odd">
 							<th scope="row">Friday<br />6:00-7:00 P.M.</th>
 							<td>Friday Club<br />(Ages 8-11)</td>
 							<td></td>
+						</tr>
+						<tr>
+							<th scope="row">Saturday<br /> 8:45 A.M.</th>
+							<td><strong>Women's Breakfast</strong>:<br />Miss Janice Reynolds</td>
+							<td>(Ticket-holders only)</td>
 						</tr>
 						<tr class="odd">
 							<th scope="row">Sunday<br />10:30 A.M.<br />6:30 P.M.</th>
