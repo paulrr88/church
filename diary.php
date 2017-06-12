ae89214d0ef2628715997dcebe05693d7dc7ac84
paulrr88 <paul@Pauls-iMac.home>
2017-06-12 10:05:33 +0100

    Weekly update

--- diary.php
+++ diary.php
@@ -6,7 +6,7 @@
 
 				<p class="centre">(For a long-term look at our church, see the <a href="upcoming.php">Coming Weeks page</a>)</p>
 
-				<p class="centre"><strong>Schedule for the week 4th to 11th June</strong></p>
+				<p class="centre"><strong>Schedule for the week 11th to 18th June</strong></p>
 
 				<table class="styled" summary="Schedule for events in the coming week(s)">
 					<thead>
@@ -19,36 +19,33 @@
 					<tbody>
 						<tr class="odd">
 							<th scope="row">Tuesday<br />2:30 P.M.</th>
-							<td>Women's Fellowship:<br />Miss Pam Evans</td>
+							<td>Women's Fellowship<br />Strawberry Tea</td>
 							<td></td>
 						</tr>
-						<tr><!--temporary-->
-							<th scope="row">Tuesday<br />7:30 P.M.</th>
-							<td><strong>"Life Explored"</strong></td>
-							<td>Looking at life in fresh ways from a<br />
-                                Christian perspective; for all<br />
-                                experience levels! All welcome,<br />
-                                no charge, refreshments included!</td>
-						</tr>
-						<tr class="odd">
-							<th scope="row">Wednesday<br />3:00-4:00 P.M.</th>
-							<td>Prayer time</td>
-							<td></td>
-						</tr>
-						<tr>
+                        <tr>
 							<th scope="row">Thursday<br /> 10:30 A.M.</th>
 							<td>Home Group at<br />Mike &amp; Pat's</td>
+							<td>Timothy - "The Young Pastor"</td>
+						</tr>
+						<tr class="odd">
+							<th scope="row">Thursday<br />2:00-4:00 P.M.</th>
+							<td>"Dabblers" Hobby Group</td>
+							<td>All welcome!</td>
+						</tr>
+						<tr>
+							<th scope="row">Friday<br />10:55 A.M.</th>
+							<td>Home Group at<br />Bryan &amp; Audrey's</td>
 							<td></td>
 						</tr>
 						<tr class="odd">
-							<th scope="row">Friday<br /><em>5:00-6:00 P.M.</em></th>
+							<th scope="row">Friday<br />5:00-6:00 P.M.</th>
 							<td>Friday Club<br />(Ages 4-11)</td>
-							<td><strong>Please note the new time!</strong></td>
+							<td></td>
 						</tr>
 						<tr>
 							<th scope="row">Sunday<br />10:30 A.M.<br />6:30 P.M.</th>
 							<td>Sunday Services</td>
-							<td>Evening worship includes communion</td>
+							<td>Morning worship includes communion</td>
 						</tr>
 					</tbody>
 				</table>
