dac7f67eebc5aad75a0f8c0ba67f8a906d8dee0e
paulrr88 <paul@Pauls-iMac.home>
2017-12-12 10:25:04 +0000

    Weekly update

--- diary.php
+++ diary.php
@@ -6,7 +6,7 @@
 
 				<p class="centre">(For a long-term look at our church, see the <a href="upcoming.php">Coming Weeks page</a>)</p>
 
-				<p class="centre"><strong>Schedule for the week 3rd to 10th December</strong></p>
+				<p class="centre"><strong>Schedule for the week 10th to 17th December</strong></p>
 
 				<table class="styled" summary="Schedule for events in the coming week(s)">
 					<thead>
@@ -18,29 +18,30 @@
 					</thead>
 					<tbody>
                         <tr class="odd">
-							<th scope="row">Tuesday<br />2:15 P.M.</th>
-							<td>Women's Fellowship: Children's Choir</td>
+							<th scope="row">Tuesday<br />2:30 P.M.</th>
+							<td>Women's Fellowship:<br />Christmas Tea</td>
+							<td></td>
+						</tr>
+						<tr>
+							<th scope="row">Thursday<br />2:00-4:00 P.M.</th>
+							<td>"Dabblers" Hobby Group</td>
 							<td>All welcome!</td>
 						</tr>
-						<tr>
-							<th scope="row">Wednesday<br />3:00-4:00 P.M.</th>
-							<td>Prayer time</td>
-							<td></td>
-						</tr>
 						<tr class="odd">
-							<th scope="row">Thursday<br /> 10:30 A.M.</th>
-							<td>Home Group at<br />Mike &amp; Pat's</td>
+							<th scope="row">Friday<br />10:55 A.M.</th>
+							<td>Home Group at<br />Bryan &amp; Audrey's</td>
 							<td></td>
 						</tr>
 						<tr>
-							<th scope="row">Friday<br />5:00-6:00 P.M.</th>
-							<td>Friday Club<br />(Ages 4-11)</td>
+							<th scope="row">Friday<br />5:00-6:30 P.M.</th>
+							<td>Friday Club<br /><strong>Christmas Party</strong></td>
 							<td></td>
 						</tr>
 						<tr class="odd">
 							<th scope="row">Sunday<br />10:30 A.M.<br />6:30 P.M.</th>
 							<td>Sunday Services</td>
-							<td></td>
+							<td><strong>Friday Club Christmas Carol Service<br />
+                                in the morning</strong></td>
 						</tr>
 					</tbody>
 				</table>
