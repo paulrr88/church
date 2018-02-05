4e999945fc71a15de5ff7370c5ab44dbb19de5bc
paulrr88 <paul@Pauls-iMac.home>
2018-02-05 10:23:23 +0000

    Weekly update

--- diary.php
+++ diary.php
@@ -6,7 +6,7 @@
 
 				<p class="centre">(For a long-term look at our church, see the <a href="upcoming.php">Coming Weeks page</a>)</p>
 
-				<p class="centre"><strong>Schedule for the week 28th January to 4th February</strong></p>
+				<p class="centre"><strong>Schedule for the week 4th to 11th February</strong></p>
 
 				<table class="styled" summary="Schedule for events in the coming week(s)">
 					<thead>
@@ -22,9 +22,19 @@
 							<td>"Faith Explored"</td>
 							<td></td>
 						</tr>
-                        <tr>
-							<th scope="row">Wednesday<br />3:00-4:00 P.M.</th>
-							<td>Prayer time</td>
+						<tr>
+							<th scope="row">Thursday<br /> 10:30 A.M.</th>
+							<td>Home Group at<br />Mike &amp; Pat's</td>
+							<td></td>
+						</tr>
+                         <tr class="odd">
+							<th scope="row">Thursday<br />2:00-4:00 P.M.</th>
+							<td>"Dabblers" Hobby Group</td>
+							<td>All welcome!</td>
+						</tr>
+						<tr>
+							<th scope="row">Friday<br />10:55 A.M.</th>
+							<td>Home Group at<br />Bryan &amp; Audrey's</td>
 							<td></td>
 						</tr>
 						<tr class="odd">
@@ -32,15 +42,13 @@
 							<td>Friday Club<br />(Ages 4-12)</td>
 							<td></td>
 						</tr>
-                        <tr>
-							<th scope="row">Saturday<br /> 8:45 A.M.</th>
-							<td><strong>Women's Breakfast</strong>:<br />Annemarijn Dijkslag</td>
-							<td>(Ticket-holders only)</td>
-						</tr>
-						<tr class="odd">
+						<tr>
 							<th scope="row">Sunday<br />10:30 A.M.<br />6:30 P.M.</th>
 							<td>Sunday Services</td>
-							<td>Evening worship includes communion</td>
+							<td>Morning service will be led by<br />
+								Rev. David White<br />
+								Evening service will be<br />
+								Fellowship Time</td>
 						</tr>
 					</tbody>
 				</table>
