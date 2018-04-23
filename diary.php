80f55c7cb0ade875559c35e3408905f09de504c0
paulrr88 <paul@Pauls-iMac.home>
2018-04-23 09:30:21 +0100

    Weekly update

--- diary.php
+++ diary.php
@@ -6,7 +6,7 @@
 
 				<p class="centre">(For a long-term look at our church, see the <a href="upcoming.php">Coming Weeks page</a>)</p>
 
-				<p class="centre"><strong>Schedule for the week 15th to 22nd April</strong></p>
+				<p class="centre"><strong>Schedule for the week 22nd to 29th April</strong></p>
 
 				<table class="styled" summary="Schedule for events in the coming week(s)">
 					<thead>
@@ -22,14 +22,14 @@
 							<td>"Faith Explored"</td>
 							<td></td>
 						</tr>
-						<tr>
-							<th scope="row">Thursday<br />2:00-4:00 P.M.</th>
-							<td>"Dabblers" Hobby Group</td>
-							<td>All welcome!</td>
+                         <tr>
+							<th scope="row">Wednesday<br />3:00-4:00 P.M.</th>
+							<td>Prayer time</td>
+							<td></td>
 						</tr>
 						<tr class="odd">
-							<th scope="row">Friday<br />10:55 A.M.</th>
-							<td>Home Group at<br />Bryan &amp; Audrey's</td>
+							<th scope="row">Thursday<br /> 10:30 A.M.</th>
+							<td>Home Group at<br />Mike &amp; Pat's</td>
 							<td></td>
 						</tr>
 						<tr>
@@ -40,7 +40,10 @@
 						<tr class="odd">
 							<th scope="row">Sunday<br />10:30 A.M.<br />6:30 P.M.</th>
 							<td>Sunday Services</td>
-							<td></td>
+							<td>Morning service will be led by<br />
+								Rev. Stuart Davison<br />
+								Evening service will be led by<br />
+								Mr. David Napper</td>
 						</tr>
 					</tbody>
 				</table>
